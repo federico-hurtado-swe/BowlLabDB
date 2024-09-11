124a75d48271ea4267438207b1e66b3442f36c2c
Federico Hurtado <fed_home@Federicos-Mac-mini.local>
2024-09-11 11:16:20 -0400

    Initial download from spring initializer.

---
+++ src/main/java/DBMS/group6/BowlLabDB/BowlLabDbApplication.java
@@ -0,0 +1,13 @@
+package DBMS.group6.BowlLabDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlLabDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlLabDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DBMS/group6/BowlLabDB/BowlLabDbApplicationTests.java
@@ -0,0 +1,13 @@
+package DBMS.group6.BowlLabDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlLabDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
