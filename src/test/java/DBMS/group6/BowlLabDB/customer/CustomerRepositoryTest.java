9040be4118367a11a40efb65a0e1e2e8b5a28290
Federico Hurtado <fed_home@Federicos-Mac-mini.local>
2024-09-19 13:18:01 -0400

    Enable CORS for front end connection and set up a container for testing to prevent test data from interfering with real data.

--- src/test/java/DBMS/group6/BowlLabDB/customer/CustomerRepositoryTest.java
+++ src/test/java/DBMS/group6/BowlLabDB/customer/CustomerRepositoryTest.java
@@ -6,30 +6,62 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import DBMS.group6.BowlLabDB.customer.models.Customer;
 
 @JdbcTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test") // Use application-test.properties during tests
 @Import(CustomerRepository.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // Ensure Spring doesn't replace your Testcontainers DataSource
 public class CustomerRepositoryTest {
+
+    // test container so that we do not add data to real DB during testing 
+    private static PostgreSQLContainer<?> testContainer = 
+            new PostgreSQLContainer<>("postgres:latest")
+                .withDatabaseName("testdb")
+                .withUsername("test")
+                .withPassword("test");
     
     @Autowired
     CustomerRepository customerRepository;
 
     private Customer customerOne;
 
+    static {
+        testContainer.start();
+    }
+
+    @BeforeAll
+    static void startContainer() {
+        testContainer.start();
+        System.setProperty("DB_URL", testContainer.getJdbcUrl());
+        System.setProperty("DB_USERNAME", testContainer.getUsername());
+        System.setProperty("DB_PASSWORD", testContainer.getPassword());
+    }
+
+    // Stop the container after all tests
+    @AfterAll
+    static void stopContainer() {
+        testContainer.stop();
+    }
+
     @BeforeEach
     void setUp() {
-        customerOne = new Customer(1, "Customer", "One", "CustomerOne@gmail.com", "111-111-1111", "PasswordOne", List.of(1), 0 );
+        
+        customerOne = new Customer(1, "Customer", "One", "CustomerOne@gmail.com", "111-111-1111", "PasswordOne", List.of(1), 0);
         customerRepository.create(customerOne);
     }
+
 
     @Test
     void createCustomer() {
